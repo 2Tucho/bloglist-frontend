32f2ff9da2f109f8d796fb0d2d222991a63c13a6
2Tucho <albergromero@gmail.com>
2025-08-20 11:45:24 +0200

    Added exercise 5.15

--- src/components/Blog.test.jsx
+++ src/components/Blog.test.jsx
@@ -1,20 +1,21 @@
 import { render, screen } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
 import Blog from "./Blog"
+import { expect } from "vitest"
+
+const blog = {
+    title: "Ardiente amor",
+    author: "Café Quijano",
+    url: "www.cafequijano.com",
+    likes: 234,
+    user: "6874d188e327452890fcf9b0",
+    id: "6874dfecb4b5294ebb401c5d"
+}
 
 test("renders title and author by default", () => {
-    const blog = {
-        title: "Ardiente amor",
-        author: "Café Quijano",
-        url: "www.cafequijano.com",
-        likes: 234,
-        user: "6874d188e327452890fcf9b0",
-        id: "6874dfecb4b5294ebb401c5d"
-    }
+    const mockHandler = vi.fn()
 
-    const setBlogs = vi.fn()
-
-    const { container } = render(<Blog blog={blog} setBlogs={setBlogs} blogs={[blog]} />)
+    const { container } = render(<Blog blog={blog} />)
 
     const div = container.querySelector(".classBlog")
     expect(div).toHaveTextContent(
@@ -23,24 +24,13 @@ test("renders title and author by default", () => {
 })
 
 test("shows URL and likes when View button is clicked", async () => {
-    const blog = {
-        title: "Ardiente amor",
-        author: "Café Quijano",
-        url: "www.cafequijano.com",
-        likes: 234,
-        user: "6874d188e327452890fcf9b0",
-        id: "6874dfecb4b5294ebb401c5d"
-    }
+    const mockHandler = vi.fn()
 
-    const setBlogs = vi.fn()
-
-    const { container } = render(<Blog blog={blog} setBlogs={setBlogs} blogs={[blog]} />)
+    const { container } = render(<Blog blog={blog} />)
 
     const user = userEvent.setup()
     const button = screen.getByText("View")
     await user.click(button)
-
-    screen.debug()
 
     expect(container).toHaveTextContent(
         "www.cafequijano.com"
@@ -48,4 +38,21 @@ test("shows URL and likes when View button is clicked", async () => {
     expect(container).toHaveTextContent(
         234
     )
+})
+
+test("when Like button is clicked it calls the event controller", async () => {
+    const mockHandleLike = vi.fn()
+
+    render(<Blog blog={blog} handleLike={mockHandleLike} />)
+
+    const user = userEvent.setup()
+    const viewButton = screen.getByText("View")
+
+    await user.click(viewButton)
+    const likeButton = screen.getByText("Like")
+
+    await user.click(likeButton)
+    await user.click(likeButton)
+
+    expect(mockHandleLike).toHaveBeenCalledTimes(2)
 })
